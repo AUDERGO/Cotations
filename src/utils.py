be99e1c570b1e55a0081992c633d991e96cc164c
AUDERGO <audrey.vernet@volvo.com>
2026-06-29 09:53:14 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,2 @@
-def clean_poste_name(file_name):
-    return (
-        file_name
-        .replace("MEC_", "")
-        .replace("VEC_", "")
-        .replace(".xlsx", "")
-    )
+def get_poste_name(file_name):
+    return file_name.replace(".xlsx", "")
