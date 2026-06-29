75679726584276ff2534188da2629cc6585f8252
AUDERGO <audrey.vernet@volvo.com>
2026-06-29 09:48:43 +0200

    Add file paths and constants to config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+from pathlib import Path
 
+BASE_PATH = Path("data")
+
+MEC_PATH = BASE_PATH / "MEC"
+VEC_PATH = BASE_PATH / "VEC"
+
+ENGINS_FILE = BASE_PATH / "cotations_engins.xlsx"
+
+OUTPUT_FILE = "cotations_bilan.xlsx"
