d993dc7dfe57be6db90b7640cc9f0e63d9bbdc1d
AUDERGO <audrey.vernet@volvo.com>
2026-06-29 09:50:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pandas as pd
+from main import run
+
+st.title("Cotation ergonomique")
+
+if st.button("Lancer le calcul"):
+    run()
+    st.success("Calcul terminé")
+
+try:
+    df = pd.read_excel("cotations_bilan.xlsx")
+
+    st.dataframe(df)
+
+    poste = st.selectbox("Choisir un poste", df["Poste"])
+
+    row = df[df["Poste"] == poste].iloc[0]
+
+    st.write("Postures", row)
+except:
+    st.info("Clique sur lancer")
 
